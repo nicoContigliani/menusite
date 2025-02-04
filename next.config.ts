d99daff65b6f3c1777095eed0aa2a1487d67b448
nicoContigliani <nico.contigliani>
2025-02-04 10:43:00 -0300

    todo

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,9 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   reactStrictMode: true,
-//   transpilePackages: ['antd', '@ant-design', 'rc-util', 'lodash-es'],
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "http",
-//         hostname: "**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "**",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactStrictMode: true,
-  transpilePackages: ['antd', '@ant-design', 'rc-util', 'lodash-es', 'rc-pagination', 'rc-picker'], // Added rc-picker
-  experimental: {
-    optimizePackageImports: ['antd', '@ant-design/icons'],
-  },
+  transpilePackages: ['antd', '@ant-design', 'rc-util', 'lodash-es'],
   images: {
     remotePatterns: [
       {
@@ -40,12 +15,6 @@ const nextConfig: NextConfig = {
         hostname: "**",
       },
     ],
-  },
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.resolve.extensions.push('.js', '.ts', '.tsx');  // Allow .js files in client-side code
-    }
-    return config;
   },
 };
 
