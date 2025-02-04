45ca020ea0b062bdad1b0183c48e0a3293777086
nicoContigliani <nico.contigliani>
2025-02-04 10:31:33 -0300

    todo

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,34 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactStrictMode: true,
+//   transpilePackages: ['antd', '@ant-design', 'rc-util', 'lodash-es'],
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "http",
+//         hostname: "**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-  transpilePackages: ['antd', '@ant-design', 'rc-util', 'lodash-es'],
+  transpilePackages: ['antd', '@ant-design', 'rc-util', 'lodash-es', 'rc-pagination'],
+  experimental: {
+    optimizePackageImports: ['antd', '@ant-design/icons'],
+  },
   images: {
     remotePatterns: [
       {
