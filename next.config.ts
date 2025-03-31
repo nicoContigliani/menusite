71109adfb269b2628bcbce43accafb78bc6b741a
nicoContigliani <nico.contigliani>
2025-03-31 16:35:23 -0300

    dockerfile

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   reactStrictMode: true,
-//   transpilePackages: ['antd', '@ant-design', 'rc-util', 'lodash-es'],
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "http",
-//         hostname: "**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "**",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
